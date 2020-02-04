1f337e1c52ee69d4169d77ab623749cb2ab37bda
ewebprovider <veeermca2010@gmail.com>
2020-02-04 18:16:32 +0530

    build

---
+++ build/precache-manifest.fe5751291b40f2684520b637b9c51be0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0c69fb874980dee7c63d9252fb0245e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f179d312e53b77c7e19d",
+    "url": "/static/css/main.d374d9eb.chunk.css"
+  },
+  {
+    "revision": "f23a9b476f042ba19994",
+    "url": "/static/js/2.ef0bb92f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.ef0bb92f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f179d312e53b77c7e19d",
+    "url": "/static/js/main.e6e331df.chunk.js"
+  },
+  {
+    "revision": "fc5415119e47b22bad9d",
+    "url": "/static/js/runtime-main.41e07dc4.js"
+  },
+  {
+    "revision": "a3d196d20d19e98ac965f446e66fc192",
+    "url": "/static/media/page-title-bg.a3d196d2.png"
+  }
+]);
